d900978d149d0ac92ec0c4e8acfdc8761c36c25b
Aleksei Chernenkov <161827261+chernenkov-ayu-sbmt@users.noreply.github.com>
2024-10-18 14:59:39 +0500

    [DEX-0000] feat: Add multi-process GRPC::RpcServer implementation

--- grpc/server.rb
+++ grpc/server.rb
@@ -1,7 +1,23 @@
 # frozen_string_literal: true
 
 require 'grpc'
+require 'logger'
 require_relative '../pb/hello_services_pb'
+
+THREADS_COUNT = 5
+WAIT_QUEUE_SIZE = 32
+BIND_ADDRESS = '0.0.0.0:9091'
+
+STDOUT.sync = true
+
+module RubyLogger
+  LOGGER = Logger.new(STDOUT)
+  def logger = LOGGER
+end
+
+module GRPC
+  extend RubyLogger
+end
 
 class HelloImpl < ::Hello::Greeter::Service
   def say_hello(req, _rpc)
@@ -9,14 +25,12 @@ class HelloImpl < ::Hello::Greeter::Service
   end
 end
 
-s = GRPC::RpcServer.new(
-  pool_size: 5,
-  poll_period: 1,
-  pool_keep_alive: 1,
-  max_waiting_requests: 32
+server = GRPC::RpcServer.new(
+  pool_size: THREADS_COUNT,
+  max_waiting_requests: WAIT_QUEUE_SIZE
 )
-s.add_http2_port('localhost:50051', :this_port_is_insecure)
-s.handle(HelloImpl.new)
+server.add_http2_port(BIND_ADDRESS, :this_port_is_insecure)
+server.handle(HelloImpl)
 
 puts 'GRPC server is starting...'
-s.run_till_terminated
+server.run_till_terminated
